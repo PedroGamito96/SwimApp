e03c4929e7eb9862b80f25a9dea27d29541e2042
PedroGamito96 <pedromvgamito@gmail.com>
2017-01-10 20:38:38 +0000

    First commit

---
+++ app/src/main/java/pt/ipbeja/pdm2/swimapp/MainActivity.java
@@ -0,0 +1,13 @@
+package pt.ipbeja.pdm2.swimapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
